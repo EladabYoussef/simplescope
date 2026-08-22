faabbe15caca3ee0f232a49fb3b33425aaf911dc
EladabYoussef <youssefladeb@gmail.com>
2026-08-22 15:08:05 +0100

    implementation of the drawbitmap fn

--- Drivers/SSD1306/Src/ssd1306_gfx.c
+++ Drivers/SSD1306/Src/ssd1306_gfx.c
@@ -1,14 +1,32 @@
 
 #include "ssd1306_gfx.h"
+#include <stdint.h>
 uint8_t draw_bitmap(uint8_t x, uint8_t y, uint8_t width, uint8_t height, uint8_t** bitmap) {
     if(x > OLED_H || y > OLED_W) {
         return 1;
     }
-    uint8_t final_x = (x + width) > OLED_W ? OLED_W : (x + width);
-    uint8_t final_y = (y + height) > OLED_H ? OLED_H : (y + height);
+    uint8_t final_x = (x + height) > OLED_H ? OLED_H : (x + height);
+    uint8_t final_y = (y + width) > OLED_W ? OLED_W : (y + width);
+    uint8_t begin_off = y % 8,end_off = final_y % 8;
+    uint8_t begin_col = y/8, final_col=final_y/8;   
 
-    for(uint8_t i = x; i < final_x; i++) {
-        memcpy(&FRAME_BUFFER[i][y / 8], &bitmap[i - x][0], (final_y - y) / 8);
+    for(uint8_t row = x; row < final_x; row++) {
+        uint8_t carry = 0;
+        uint8_t mask = 0xFFu & ((1<<(8-begin_off)) - 1);
+        
+        for(uint8_t col = begin_col; col < final_col; col++) {
+            carry |= (bitmap[row-x][col-begin_col] >> begin_off);
+            FRAME_BUFFER[row][col] &= ~(mask);
+            FRAME_BUFFER[row][col] |= carry;
+            mask = 0xFFu;
+            carry = (bitmap[row-x][col-begin_col] << (8-begin_off));
+        }
+        mask = 0xFFu & ~((1<<(8-end_off)) - 1);
+        carry |= (bitmap[row-x][col-begin_col]>>begin_off) & mask;
+        FRAME_BUFFER[row][final_col] &= ~(mask);
+        FRAME_BUFFER[row][final_col] |= carry;
     }
+    if(final_x != x+height || final_y != y+width) return 1;
+
     return 0;
 }
